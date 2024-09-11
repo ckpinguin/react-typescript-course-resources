844f0bc9efd7bdf8a98d0673ce90fec05688f4c2
Christof Kälin <christof.kaelin@protonmail.com>
2024-09-11 12:39:39 +0200

    Infobox

--- code/03 React TypeScript Essentials/01 Starting Project/src/components/CourseGoalList.tsx
+++ code/03 React TypeScript Essentials/01 Starting Project/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import { type ReactNode } from "react";
 import { CourseGoal as Goal } from "../types";
 import CourseGoal from "./CourseGoal";
+import InfoBox from "./InfoBox";
 
 interface CourseGoalListProps {
   courseGoals: Goal[];
@@ -10,19 +12,36 @@ export default function CourseGoalList({
   courseGoals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (courseGoals.length === 0) {
+    return <InfoBox mode="hint">No goals found. Maybe add one?</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (courseGoals.length > 4) {
+    warningBox = (
+      <InfoBox mode="warning">
+        You have a lot of goals. Don't pressure yourself too much.
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {courseGoals.map((goal) => (
-        <li>
-          <CourseGoal
-            key={goal.id}
-            id={goal.id}
-            title={goal.title}
-            onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {courseGoals.map((goal) => (
+          <li>
+            <CourseGoal
+              key={goal.id}
+              id={goal.id}
+              title={goal.title}
+              onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
---
+++ code/03 React TypeScript Essentials/01 Starting Project/src/components/InfoBox.tsx
@@ -0,0 +1,22 @@
+import { type ReactNode } from "react";
+
+interface InfoBoxProps {
+  mode: "warning" | "hint";
+  children: ReactNode;
+}
+
+export default function InfoBox({ mode, children }: InfoBoxProps) {
+  if (mode === "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+  return (
+    <aside className="infobox infobox-warning warning--medium">
+      <h2>Warning</h2>
+      <p>{children}</p>
+    </aside>
+  );
+}
